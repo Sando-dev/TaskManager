f8f1a36eedf8e3fb6d09a100d63aacad11eb0e0a
Bauti Corti <bautistamariacorti@gmail.com>
2025-03-02 16:06:52 -0300

    models added, gitignore added

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,13 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+#configura la base de datos SQLite
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///tasks.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
 
 @app.route("/")
 def hello_world():
